6d916902ac19549ac319fdca19ef13b27fe72be0
Cfroz <charlietrestini@hotmail.com>
2024-01-05 09:17:38 +0100

    initial

---
+++ src/container/IntroAnim/IntroAnim.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./IntroAnim.scss"
+
+function IntroAnim() {
+  return (
+    <>
+        <div className='app__intro'>
+            <div className='app__introName'>
+                <span className='app__introNameOne'>Mél</span>
+                <span className='app__introNameTwo'>ina</span>
+            </div>
+        </div>
+
+        <div className='app__introTail'>
+
+        </div>
+    </>
+  )
+}
+
+export default IntroAnim
